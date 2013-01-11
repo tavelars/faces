a2c35ab726e478dde60ab57b590141dd70c94698
Thiago Avelar <tavelars@gmail.com>
2013-01-11 16:50:44 -0200

    Inicio projeto curso JSF 11_01_2013 2 parte

---
+++ semeru_jsf_maven/src/main/java/br/com/semeru/semeru/i18n/I18N.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.semeru.semeru.i18n;
+
+/**
+ *
+ * @author Admin
+ */
+public class I18N {
+    
+}
